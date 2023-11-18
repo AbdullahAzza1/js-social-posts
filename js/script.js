953a880b3667aefcff8871c1def1e7676ff3d754
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-11-18 18:20:31 +0100

    step1

--- js/script.js
+++ js/script.js
@@ -55,3 +55,38 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+let container = document.getElementById("container");
+container.innerHTML = "";
+
+let postTemplate = `<div class="post">
+        < div class="post__header" >
+            <div class="post-meta">
+                <div class="post-meta__icon">${getProfilePicture(image, name)}</div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${name}</div>
+                    <div class="post-meta__time">${convertDate(created)}</div>
+                </div>
+            </div>
+        </div >
+        <div class="post__text">${content}</div>
+        <div class="post__image">
+            <img src="${media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                     </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${id}" class="js-likes-counter">${likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div >`;
+
+container.innerHTML = postTemplate;
